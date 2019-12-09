46346944831585f86e2fa236257d11f2f09f8f8e
順也 <ij777@live.jp>
2019-12-09 16:15:45 +0900

    動画ドロップファイル取得

--- divitage/converterMain.xaml.cs
+++ divitage/converterMain.xaml.cs
@@ -20,9 +20,65 @@ namespace divitage
     /// </summary>
     public partial class converterMain : Page
     {
+        private string[] fileNames;
+        private int fileCount; //全体のファイル数
+        private int counter;   //ファイルのカウンター
         public converterMain()
         {
             InitializeComponent();
+        }
+
+        private void Grid_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            //前処理
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Copy;
+            else
+                e.Effects = DragDropEffects.None;
+        }
+
+        private void Grid_PreviewDrop(object sender, DragEventArgs e)
+        {
+            this.fileNames = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            MessageBox.Show(fileNames[0]);
+            MessageBox.Show(fileNames[1]);
+        }
+
+        private void verifyMovieFile()
+        {
+            //パラメータ初期化
+            this.fileCount = 0;
+            //処理後ファイル数
+            var afterProcessFileNames = new List<string>();
+            var errorFileName = new List<string>();
+            //対応ファイル拡張子
+            string[] allowExtensions = { ".avi", ".mp4", ".mov", ".wmv", ".flv", ".mpg" };
+            //拡張子調査・情報取得
+            foreach (string item in this.fileNames)
+            {
+                try
+                {
+                    string tmpExtension = System.IO.Path.GetExtension(item);
+                    if (0 > Array.IndexOf(allowExtensions, tmpExtension))
+                    {
+                        //対応拡張子
+                        errorFileName.Add("非対応形式であるため，次のファイルが処理できませんでした\n" + System.IO.Path.GetFileName(item));
+                        continue;
+                    }
+                    afterProcessFileNames.Add(item);
+                    MessageBox.Show("30");
+                }
+                catch (Exception err)
+                {
+                    errorFileName.Add("「" + err.Message + "」のため，次のファイルが処理できませんでした\n" + System.IO.Path.GetFileName(item));
+                    continue;
+                }
+
+
+            }
+            this.fileCount = afterProcessFileNames.Count;
+            MessageBox.Show(this.fileCount.ToString());
+
         }
     }
 }
